d69962f75f74161163fefa10faa617ad73db0fda
kenshinji <gundam0083ster@gmail.com>
2016-08-08 05:08:43 +0000

    update setup.sh to support link .tmux.conf file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash 
  
 #First find out if .tmux.conf exists under $HOME 
+
+current_dir="$( pwd )"
+
 if [ -f ~/.tmux.conf ]; then 
-	echo "tmux.conf file found!"
+	if [ -L ~/.tmux.conf ]; then
+		echo ".tmux.conf is a symbol link and now unlinking it...... "
+		unlink ~/.tmux.conf
+	else
+		echo ".tmux.conf already exists, now backup it......"
+		mv ~/.tmux.conf ~/.tmux.conf-`date +%Y%m%d`
+	fi
 else
-	echo "no tmux.conf file found!"
+	echo "No tmux.conf file found!"
 fi
 
+echo "Now creating symbol link for .tmux.conf ......"
+ln -s $current_dir/.tmux.conf ~/.tmux.conf
+echo ".tmux.conf link created!"
